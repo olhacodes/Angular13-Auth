b203fa24df8624a6b78763d1a8c27ef99292c16d
olha.codes <57961694+olhacodes@users.noreply.github.com>
2022-11-01 11:53:17 +0100

    added html contacts

--- src/app/components/admin/components/contacts/contacts.component.ts
+++ src/app/components/admin/components/contacts/contacts.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: []
 })
 export class ContactsComponent implements OnInit {
 
